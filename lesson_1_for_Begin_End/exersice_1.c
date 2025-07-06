11043c34f6c245707dbb3a5d67d8139f977ab8e0
maksympash-code <maksympash@knu.ua>
2025-07-06 21:16:47 +0300

    lesson_1_for_Begin_End--variables, operations and display

---
+++ lesson_1_for_Begin_End/exersice_1.c
@@ -0,0 +1,18 @@
+//
+// Created by maksy on 06.07.2025.
+//
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    float v;
+    t = 15;
+    t = (t - 2) * t + 5;
+    v = 2.54f;
+    v = t;
+    printf("%d \n" , t);
+    printf("%.2f", v);
+
+    return 0;
+}
--- lesson_1_for_Gosha_Dudar/exersice_4.c
+++ lesson_1_for_Gosha_Dudar/exersice_4.c
@@ -2,27 +2,27 @@
 #include "exersice_4.h"
 #include <stdio.h>
 
-int main() {
-    // const short a = 0;
-    float a, b, c;
-    scanf("%f %f", &a, &b);
-
-    c = a / b;
-
-    printf("Result: %.2f \n", c);
-
-    double x = 5.5f, y = 6.67f, res;
-
-    res = x / y;
-    printf("Result: %.2f \n", res);
-
-    // long int
-    // printf - Ld
-
-    // int, short
-    // printf - d
-
-    char s = 'S';
-    printf("%c", s);
-    return 0;
-}
+// int main() {
+//     // const short a = 0;
+//     float a, b, c;
+//     scanf("%f %f", &a, &b);
+//
+//     c = a / b;
+//
+//     printf("Result: %.2f \n", c);
+//
+//     double x = 5.5f, y = 6.67f, res;
+//
+//     res = x / y;
+//     printf("Result: %.2f \n", res);
+//
+//     // long int
+//     // printf - Ld
+//
+//     // int, short
+//     // printf - d
+//
+//     char s = 'S';
+//     printf("%c", s);
+//     return 0;
+// }
