c57a2218be5143ebbbaf2b585f4bc9232b7132dc
maksympash-code <maksympash@knu.ua>
2025-07-12 22:48:40 +0300

    lesson_3_for_Gosha_Dudar

---
+++ lesson_3_for_Gosha_Dudar/exercise_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// int main() {
+//     int x = 20, y = 5;
+//
+//     if ((x == 10 && y < 7) || x == 20)
+//         printf("Yes\n");
+//     else if (x > 100)
+//         printf("X is greater 100\n");
+//     else if (x < 5) {
+//         printf("X is less 5\n");
+//     }
+//     else
+//         printf("No\n");
+//
+//
+//     bool isHasCar = true;
+//     if (isHasCar)
+//         printf("Yes\n");
+//     else
+//         printf("No\n");
+//
+//     return 0;
+// }
