2f7f769f1f74559c50a4a57fc75bd942b737205b
maksympash-code <maksympash@knu.ua>
2025-07-14 21:55:17 +0300

    lesson_5_for_Gosha_Dudar_function_is_C

---
+++ lesson_5_for_Gosha_Dudar/exercise_1.c
@@ -0,0 +1,35 @@
+#include "functions.h"
+
+void sum(int a, int b);
+float mult(float a, float b);
+void string(char arr[]);
+
+int main() {
+    sum(20, 97);
+    int x = 10, y = 34;
+    sum(x, y);
+
+    string("Hello");
+    char word[] = {'s', 'o', 'm', 'e'};
+    string(word);
+
+    float result = mult(4.5f, 7.8f);
+    printf("Result: %.2f\n", result);
+
+    return 0;
+}
+
+void sum(int a, int b) {
+    int res = a + b;
+    printf("%d\n", res);
+}
+
+void string(char arr[]) {
+    printf("%s\n", arr);
+}
+
+float mult(float a, float b) {
+    float res = a *b;
+    printf("Result: %f\n", res);
+    return res;
+}
---
+++ lesson_5_for_Gosha_Dudar/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdio.h>
+
+void info() {
+    printf("Привет\n");
+    printf("всем\n");
+    printf("!\n");
+}
+
+#endif //FUNCTIONS_H
