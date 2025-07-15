c3dbeea7cf54b02828fe9eb7bac3be32fb8a4957
maksympash-code <maksympash@knu.ua>
2025-07-15 22:26:13 +0300

    lesson_7_for_Begin_End_symbols, strings, libraries

---
+++ lesson_7_for_Begin_End/exercise_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <windows.h> // <windows.h
+#include <winuser.h>
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // char ch = 'W';
+    // printf("%c\n", ch);
+    //
+    // ch = getch();
+    // printf("ch = %c", ch);
+
+    char s[10] = "фауаау";
+    s[0] = '7';
+    printf("%s\n", s);
+
+    sprintf(s, "12121212");
+    s[4] = '\0';
+    printf("%s\n", s);
+
+    gets(s);
+    OemToChar(s, s);
+    printf("%s\n", s);
+
+    return 0;
+}
