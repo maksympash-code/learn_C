69b610aaef3595d2f08e27821db8cd614e6a2d22
maksympash-code <maksympash@knu.ua>
2025-07-17 22:09:49 +0300

    lesson_6_for_Gosha_Dudar_Struchers

--- lesson_9_for_Begin_End/exercise_1.c
+++ lesson_9_for_Begin_End/exercise_1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int arr[9][9];
-    arr[2][5] = 3;
-
-    for (int k = 0; k < 9; k ++) {
-        for (int i = 0; i < 9; i++) {
-            arr[k][i] = 7;
-            printf("%d\t", arr[k][i]);
-        }
-        printf("\n");
-    }
-
-    char arr3[2][6][20];
-    arr3[1][2][10] = "W";
-
-    return 0;
-}
+// int main() {
+//     int arr[9][9];
+//     arr[2][5] = 3;
+//
+//     for (int k = 0; k < 9; k ++) {
+//         for (int i = 0; i < 9; i++) {
+//             arr[k][i] = 7;
+//             printf("%d\t", arr[k][i]);
+//         }
+//         printf("\n");
+//     }
+//
+//     char arr3[2][6][20];
+//     arr3[1][2][10] = "W";
+//
+//     return 0;
+// }
