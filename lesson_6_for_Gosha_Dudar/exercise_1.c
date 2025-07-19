75aa55d70aa6faa6748eea9a1804489a62d98e68
maksympash-code <maksympash@knu.ua>
2025-07-19 17:04:44 +0300

    lesson_9_for_Begin_End_do_homework

--- lesson_6_for_Gosha_Dudar/exercise_1.c
+++ lesson_6_for_Gosha_Dudar/exercise_1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Car {
-    int speed;
-    char name[50];
-    float weight;
-};
-
-int main() {
-
-    struct Car bmw;
-    bmw.speed = 175;
-    strcpy(bmw.name, "BMW M5");
-    bmw.weight = 2500.00f;
-
-    struct Car audi = {250, "Audi R8", 2700.00f};
-    printf("%s his speed %d\n", audi.name, audi.speed);
-    printf("%s his speed %d\n", bmw.name, bmw.speed);
-
-    return 0;
-}
+// struct Car {
+//     int speed;
+//     char name[50];
+//     float weight;
+// };
+//
+// int main() {
+//
+//     struct Car bmw;
+//     bmw.speed = 175;
+//     strcpy(bmw.name, "BMW M5");
+//     bmw.weight = 2500.00f;
+//
+//     struct Car audi = {250, "Audi R8", 2700.00f};
+//     printf("%s his speed %d\n", audi.name, audi.speed);
+//     printf("%s his speed %d\n", bmw.name, bmw.speed);
+//
+//     return 0;
+// }
---
+++ lesson_9_for_Begin_End/HW/exersice_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// int main() {
+//     int arr[9][9];
+//
+//     for (int k = 1; k < 9; k++){
+//         for (int i = 1; i < 9; i++) {
+//             arr[k][i] = i * k;
+//             printf("%d * %d = %d\t", k, i, arr[k][i]);
+//         }
+//         printf("\n");
+//     }
+//
+//
+//     return 0;
+// }
