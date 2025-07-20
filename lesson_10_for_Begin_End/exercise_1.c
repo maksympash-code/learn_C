e9cb98a8a5b394879fb7afc25885ae4414c80dcc
maksympash-code <maksympash@knu.ua>
2025-07-20 16:09:13 +0300

    lesson_10_for_Begin_End_first_game_dog_and_apple

---
+++ lesson_10_for_Begin_End/exercise_1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    char location[10][21];
+    int x = 10, y = 5;
+    char key;
+    int ox, oy;
+    int ax = 5, ay = 3;
+    int count = 0;
+
+    srand(time(NULL));
+
+
+    do {
+        sprintf(location[0], "###################");
+        for (int i = 1; i < 9; i++)
+            sprintf(location[i], "#                 #");
+        sprintf(location[9], "###################");
+
+
+        location[y][x] = '@';
+        location[ay][ax] = '*';
+
+        system("cls");
+        printf("count = %d\n", count);
+        for (int i = 0; i < 10; i++) {
+            printf("%s\n", location[i]);
+        }
+
+        key = getch();
+
+        ox = x;
+        oy = y;
+        if (key == 'w')
+            y--;
+        if (key == 's')
+            y++;
+        if (key == 'a')
+            x--;
+        if (key == 'd')
+            x++;
+        if (location[y][x] == '#') {
+            x = ox;
+            y = oy;
+        }
+
+        if ((x == ax) && (y == ay)) {
+            count++;
+            ax = rand() * 1.0f / RAND_MAX * 17 + 1;
+            ay = rand() * 1.0f / RAND_MAX * 7 + 1;
+        }
+    }   while (key != 'e');
+
+    return 0;
+}
--- lesson_9_for_Begin_End/HW/exercise_2.c
+++ lesson_9_for_Begin_End/HW/exercise_2.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    int arr[9][9];
-
-    for (int k = 1; k < 9; k++) {
-        for (int i = 1; i < 9; i++) {
-            arr[k][i] = 7;
-
-            if (i == 5)
-                arr[k][5] = 5;
-            if (k == 3)
-                arr[3][i] = 3;
-
-            printf("%d\t", arr[k][i]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// int main() {
+//     int arr[9][9];
+//
+//     for (int k = 1; k < 9; k++) {
+//         for (int i = 1; i < 9; i++) {
+//             arr[k][i] = 7;
+//
+//             if (i == 5)
+//                 arr[k][5] = 5;
+//             if (k == 3)
+//                 arr[3][i] = 3;
+//
+//             printf("%d\t", arr[k][i]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
