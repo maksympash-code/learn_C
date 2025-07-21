9be7eb89737638235f6a8048f8593d98be67e4d8
maksympash-code <maksympash@knu.ua>
2025-07-21 14:03:50 +0300

    lesson_13_for_Begin_End_itoa_atoi

--- lesson_12_for_Begin_End/HW/exercise_2.c
+++ lesson_12_for_Begin_End/HW/exercise_2.c
@@ -7,35 +7,35 @@ struct Thing {
     char color[250];
 } thing;
 
-int main() {
-
-    struct Thing t[3];
-
-    t[0].length = 9.7f;
-    t[0].height = 4.5f;
-    t[0].width = 2.7f;
-
-    strcpy(t[0].name, "pencil case");
-    strcpy(t[0].color, "blue");
-
-    t[1].length = 3.0f;
-    t[1].height = 2.2f;
-    t[1].width = 1.4f;
-
-    strcpy(t[1].name, "pen");
-    strcpy(t[1].color, "black");
-
-    t[2].length = 4.4f;
-    t[2].height = 12.1f;
-    t[2].width = 3.8f;
-
-    strcpy(t[2].name, "a cap of tea");
-    strcpy(t[2].color, "brown");
-
-
-    for (int i = 0; i < 3; i++) {
-        printf("%.2f %.2f %.2f\n%s\n%s\n", t[i].length, t[i].height, t[i].width, t[i].name, t[i].color);
-    }
-
-    return 0;
-}
+// int main() {
+//
+//     struct Thing t[3];
+//
+//     t[0].length = 9.7f;
+//     t[0].height = 4.5f;
+//     t[0].width = 2.7f;
+//
+//     strcpy(t[0].name, "pencil case");
+//     strcpy(t[0].color, "blue");
+//
+//     t[1].length = 3.0f;
+//     t[1].height = 2.2f;
+//     t[1].width = 1.4f;
+//
+//     strcpy(t[1].name, "pen");
+//     strcpy(t[1].color, "black");
+//
+//     t[2].length = 4.4f;
+//     t[2].height = 12.1f;
+//     t[2].width = 3.8f;
+//
+//     strcpy(t[2].name, "a cap of tea");
+//     strcpy(t[2].color, "brown");
+//
+//
+//     for (int i = 0; i < 3; i++) {
+//         printf("%.2f %.2f %.2f\n%s\n%s\n", t[i].length, t[i].height, t[i].width, t[i].name, t[i].color);
+//     }
+//
+//     return 0;
+// }
---
+++ lesson_13_for_Begin_End/exercise_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <tgmath.h>
+#include <stdlib.h>
+
+int main() {
+
+    char c = 105;
+    printf("%c\n", c);
+
+    return 0;
+}
