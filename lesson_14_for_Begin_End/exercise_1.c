74d6b057763545097f6940a8d58b9909e6d9c2f8
maksympash-code <maksympash@knu.ua>
2025-07-23 18:33:40 +0300

    lesson_17_for_Begin_End_do_arkanoid

--- lesson_14_for_Begin_End/exercise_1.c
+++ lesson_14_for_Begin_End/exercise_1.c
@@ -1,24 +1,94 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
+#include <tgmath.h>
+#include <math.h>
+#include <stdbool.h>
 
 #define width 65
 #define height 25
+
+void moveBall(float x, float y);
 
 typedef struct {
     int x, y;
     int w;
 } TRacket;
 
+typedef struct {
+    float x, y;
+    int ix, iy;
+    float alpha;
+    float speed;
+} TBall;
+
 char mas[height][width + 1];
 TRacket racket;
+TBall ball;
+int hitCnt = 0;
+int maxHitCnt = 0;
 
 void initRacket() {
     racket.w = 7;
     racket.x = (width - racket.w) / 2;
     racket.y = height - 1;
+}
+
+void initBall() {
+    moveBall(2, 2);
+    ball.alpha = -1;
+    ball.speed = 0.5f;
+}
+
+void PutBall() {
+    mas[ball.iy][ball.ix] = '*';
+}
+
+void moveBall(float x, float y) {
+    ball.x = x;
+    ball.y = y;
+    ball.ix = (int) round(ball.x);
+    ball.iy = (int) round(ball.y);
+}
+
+void autoMoveBall() {
+    double pi = M_PI;
+
+    if (ball.alpha < 0) 
+        ball.alpha +=  pi * 2;
+    if (ball.alpha > pi * 2)
+        ball.alpha -= pi * 2;
+
+    TBall bl = ball;
+    moveBall(ball.x + cos(ball.alpha) * ball.speed,
+        ball.y + sin(ball.alpha) * ball.speed);
+
+    if ((mas[ball.iy][ball.ix] == '#') || mas[ball.iy][ball.ix] == '@'){
+
+        if (mas[ball.iy][ball.ix] == '@')
+            hitCnt += 1;
+
+        if ((ball.ix != bl.ix) && (ball.iy != bl.iy)){
+            if (mas[bl.iy][ball.ix] == mas[ball.iy][bl.ix])
+                bl.alpha = bl.alpha + pi;
+            else{
+                if (mas[bl.iy][ball.ix] == '#')
+                    bl.alpha = (2 * pi - bl.alpha) + pi;
+                else
+                    bl.alpha = (2 * pi - bl.alpha);
+            }
+        }
+        else if (ball.iy == bl.iy)
+            bl.alpha = (2 * pi - bl.alpha) + pi;
+        else
+            bl.alpha = (2 * pi - bl.alpha);
+        
+        ball = bl;
+        autoMoveBall();
+    }
 }
 
 void PutRacket() {
@@ -40,12 +110,16 @@ void init() {
     for (int i = 2; i < height; i++) {
         strncpy(mas[i], mas[1], width + 1);
     }
-    }
+}
 
 
 void show() {
     for (int i = 0; i < height; i++) {
         printf("%s", mas[i]);
+        if (i == 3)
+            printf("    hit %i  ", hitCnt);
+        if (i == 4)
+            printf("    max %i  ", maxHitCnt);
         if (i < height - 1)
             printf("\n");
     }
@@ -71,13 +145,26 @@ void setCur(int x, int y) {
 int main() {
 
     char c;
+    bool run = false;
     initRacket();
+    initBall();
 
     do {
         setCur(0, 0);
+
+        if (run)
+            autoMoveBall();
+        if (ball.iy > height){
+            run = false;
+            if (hitCnt > maxHitCnt)
+                maxHitCnt = hitCnt;
+            hitCnt = 0;
+        }
+
         init();
 
         PutRacket();
+        PutBall();
         show();
 
 
@@ -85,6 +172,10 @@ int main() {
             moveRacket(racket.x - 1);
         if (GetKeyState('D') < 0)
             moveRacket(racket.x + 1);
+        if (GetKeyState('W') < 0)
+            run = true;
+        if (!run)
+            moveBall(racket.x + racket.w / 2, racket.y - 1);
         Sleep(10);
     } while (GetKeyState(VK_ESCAPE) >= 0);
 
